e48ed0897430937b7f185cd2349a3148112fb971
michael manga <middleweightsoul@gmail.com>
2021-05-10 18:56:17 +0200

    updating Response call

--- index.php
+++ index.php
@@ -3,9 +3,15 @@
   use CoverBuilder\Http\Response;
 
 
-  require_once __DIR__ . /vendor/autoload.php;
+  require_once __DIR__ . '/vendor/autoload.php';
 
   $response = new Response();
+
+  //header('Content-type: text/html');
+  //http_response_code(200);
+  
   $response->setHeaders(['Content-Type' => 'text/html']);
   $response->setStatusCode(200);
+
+  
 
